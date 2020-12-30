2d0a33d4e6565cfd4d43cd318a11b3c0a23bfcda
david <david@david.com>
2020-12-30 12:24:25 -0800

    Added some about styling with shadow box

--- components/navit.js
+++ components/navit.js
@@ -3,31 +3,22 @@ import { NavLink } from 'react-router-dom';
 import '../styles/navit.css';
 import fluff from '../images/fluff.png';
 import fluffy from '../images/fluffy.png';
+import insta from '../images/insta.svg'
+import fb from '../images/fb.svg'
 
 const Navit = () => (
   <nav className="navit">
-    <div className="div1">
     <NavLink to="/">
       <img
         src={fluff}
-        className="mb-4 mt-4 responsive"
+        className="mb-4 mt-4 responsive padleft"
       />
       <img
         src={fluffy}
         className="mb-4 mt-4 responsive-wide"
       />
     </NavLink>
-    </div>
 
-    <div className="div2">
-    <NavLink
-      exact
-      activeClassName="navit__link--active"
-      className="navit__link mb-4 mt-4"
-      to="/"
-    >
-      Home
-    </NavLink>
     <NavLink
       activeClassName="navit__link--active"
       className="navit__link mb-4 mt-4"
@@ -56,7 +47,32 @@ const Navit = () => (
     >
       Studs
     </NavLink>
-    </div>
+    <NavLink
+      activeClassName="navit__link--active"
+      className="navit__link mb-4 mt-4"
+      to="/waitlist"
+    >
+      Waitlist
+    </NavLink>
+    <NavLink
+      activeClassName="navit__link--active"
+      className="navit__link mb-4 mt-4"
+      to="/pricing"
+    >
+      Pricing
+    </NavLink>
+    <a href="https://www.instagram.com/fluffyfamilyfrenchies">
+      <img
+        src={insta}
+        className="navit__link mb-4 mt-4"
+      />
+    </a>
+    <a href="https://www.facebook.com/fluffyfamilyfrenchies">
+      <img
+        src={fb}
+        className="navit__link mb-4 mt-4 padright"
+      />
+    </a>
   </nav>
 );
 
--- pages/About.js
+++ pages/About.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import pups from '../images/pups.jpg'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import '../styles/About.css'
 
 class About extends React.PureComponent {
   render() {
     return (
-      <div>About Works!</div>
+      <Container>
+      <Row className="spacing"></Row>
+      <Row>
+	    <Col className="shadow about__text">
+		      <Image 
+		      src={pups} 
+		      className="resp"
+		      rounded />
+		      <div className="pad">
+		      Based in phoenix, Fluffy Family Frenchies produces high quality french bulldogs with health in mind. All our frenchies are guarenteed to be four panel clear so they
+		      can have a long, healthy, and happy life with their new family - you! We produce a variety of different colors in FLUFFY and shorthair! We also do stud services both
+		      in person and shipped frozen. All our pups have amazing structure and are well socialized with other dogs, cats and kids. We do not use kennels and all our dogs are strictly indoors which is why
+		      it's called fluffy FAMILY frenchies, because they are a part of our family and we raise every single puppy with that in mind. Take a look around to see the parents, available puppies, future and
+		      past litters. We hope you find your new best friend with us.
+		      </div>
+	    </Col>
+	  </Row>
+	</Container>
     );
   }
 }
