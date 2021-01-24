adab6ae53369a888f9328c54636f79bcf16cd035
david <david@david.com>
2021-01-24 15:29:51 -0800

    working image grabbing

--- src/pages/Available.js
+++ src/pages/Available.js
@@ -3,7 +3,34 @@ import '../styles/Available.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import ReactHtmlParser from 'react-html-parser'; 
 
+
+  function getAvail() {
+  	var list = arguments[0];
+
+  	if (list.length > 0){
+  		var html = '<div class="info__text"><div class="row"><div class="col-md-2 col-2"></div>';
+  		var count = 0;
+  		
+  		list.forEach(function(entry) {
+  			html += '<div class="col-md-4 col-4">';
+  			html += '<img class="wide" src="http://localhost:5000/static/' + entry.name + '/' + entry.name + '-1.jpg">';
+  			html += '</br></br><hr>';
+		    html += 'Name: ' + entry.name + '</br>';
+		    html += 'Birthday: ' + entry.birthdate + '</br>';
+		    html += 'Color: ' + entry.color + '</br>';
+		    html += entry.description + '</br>';
+		    html += '</div>';
+		});
+
+
+  		html = html + '</div></div>'
+  		return html;
+  	}
+
+
+  }
 
 class Available extends React.PureComponent {
 
@@ -27,11 +54,9 @@ class Available extends React.PureComponent {
     .then(data => this.setState({ list : data.available }))
   }
 
+
   render() {
   	const {list} = this.state;
-  	if (list.length > 0){
-  		console.log("test " + list[0].name);
-  	}
 
     return (
       <Container fluid>
@@ -41,15 +66,7 @@ class Available extends React.PureComponent {
 	    <Col xs={6} md={10} className="available__box box__color">
 	    	  <h1 className="mt-2 available__text"> Available </h1>
 	    	  <hr/>
-                  {
-                  	list.map((item) => {
-                  		return(
-                  			<div>
-                  				{item.name}
-                  			</div>
-                  		);
-                  	})
-                  }
+	    	  		{ReactHtmlParser (getAvail(list))}
 	      
 				    	  
 	    </Col>
@@ -58,7 +75,7 @@ class Available extends React.PureComponent {
 	  <Row className="available__box__pad">
       	<Col></Col>
 	    <Col xs={6} md={10} className="available__box box__color">
-	    	  <h1 className="mt-2 available__text"> .. </h1>
+	    	  <h1 className="mt-2 available__text"> Expected </h1>
 	    	  <hr/>
 	    	  <p> Text </p> 
 	    </Col>
@@ -67,7 +84,7 @@ class Available extends React.PureComponent {
 	  <Row className="available__box__pad">
       	<Col></Col>
 	    <Col xs={6} md={10} className="available__box box__color">
-	    	  <h1 className="mt-2 available__text"> .. </h1>
+	    	  <h1 className="mt-2 available__text"> Past Pups </h1>
 	    	  <hr/>
 	    	  <p> Text </p> 
 	    </Col>
